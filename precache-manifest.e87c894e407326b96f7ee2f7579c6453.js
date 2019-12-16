601942a2a92d5bce7ff67b4c62127e9c782f070f
Des_Flex <denilay13902@gmail.com>
2019-12-16 13:45:09 +0500

    Updates

---
+++ precache-manifest.e87c894e407326b96f7ee2f7579c6453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d8a0d626d4d61c517cb52bb7c26616",
+    "url": "/gh-pages-test.git/index.html"
+  },
+  {
+    "revision": "5580ddde216b9fb71fcd",
+    "url": "/gh-pages-test.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41ac78bd74ea657da4cb",
+    "url": "/gh-pages-test.git/static/js/2.4f0311a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gh-pages-test.git/static/js/2.4f0311a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5580ddde216b9fb71fcd",
+    "url": "/gh-pages-test.git/static/js/main.73fce101.chunk.js"
+  },
+  {
+    "revision": "0ceecf24b7f317f082f6",
+    "url": "/gh-pages-test.git/static/js/runtime-main.03033365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-test.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
