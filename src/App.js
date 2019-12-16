f6d942017f9128287ba3d844bd8d9c1a0e0ca6b9
Des_Flex <denilay13902@gmail.com>
2019-12-16 13:28:07 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>i love canvas</code> check it.
         </p>
         <a
           className="App-link"
